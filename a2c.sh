8057d66869c38a01571d58c4058b7c5f10a08995
Jibril-Frej <frejjibril@gmail.com>
2023-12-17 19:08:07 +0100

    update shell

--- a2c.sh
+++ a2c.sh
@@ -1,6 +1,5 @@
 for k in 1 2 3 4 5
 do
-    echo "k = $k"
-    python src/recommendation/pipeline.py --config config/dataset_V2.yaml --threshold 0.8 --model a2c -k $k --total_steps 1000000 --eval_freq 100000 --nb_runs 3
+    python src/recommendation/pipeline.py --config config/dataset_V2.yaml --threshold 0.8 --model a2c -k $k --total_steps 1000000 --eval_freq 100000 --nb_runs 3> data/V2/results/stdout_a2c_$k &
 
 done
