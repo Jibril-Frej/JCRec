95bd4fd50e35cb568959ce8918bfa89f5c445355
Jibril-Frej <frejjibril@gmail.com>
2023-12-17 23:12:38 +0100

    final?

--- optimal.sh
+++ optimal.sh
@@ -1,7 +1,8 @@
 for k in 1 2
 do
     echo "k = $k"
-    python src/recommendation/pipeline.py --config config/dataset_V2.yaml --threshold 0.8 --model optimal -k $k --total_steps 1000000 --eval_freq 100000 --nb_runs 1
+    python src/recommendation/pipeline.py --config config/dataset_V2_all.yaml --threshold 0.8 --model optimal -k $k --total_steps 1000000 --eval_freq 100000 --nb_runs 1 > data/V2/results/stdout_optimal_all_$k &
+
 
 done
 ```
