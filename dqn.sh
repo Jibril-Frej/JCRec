e52fd88fdc70ad0331bbd29c4c80a4c01b572415
Jibril-Frej <frejjibril@gmail.com>
2023-12-17 19:15:04 +0100

    fix shell

--- dqn.sh
+++ dqn.sh
@@ -1,5 +1,5 @@
 for k in 1 2 3 4 5
 do
-    python src/recommendation/pipeline.py --config config/dataset_V2.yaml --threshold 0.8 --model dqn -k $k --total_steps 1000000 --eval_freq 100000 --nb_runs 1 > data/V2/results/stdout_dqn_$k &
+    python src/recommendation/pipeline.py --config config/dataset_V2.yaml --threshold 0.8 --model dqn -k $k --total_steps 1000000 --eval_freq 100000 --nb_runs 3 > data/V2/results/stdout_dqn_$k &
 
 done
